7a76eccf98daa5bd9e52002f9f4f848a53623273
ricardomyt98 <37016050+ricardomyt98@users.noreply.github.com>
2018-06-20 07:13:20 -0300

    Create default_topology_generator.py

---
+++ default_topology_generator.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import sys
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink
+
+#Global variables
+net = Mininet(controller=Controller)
+s = 1
+
+#Functions
+def serial_connection(switches):
+	for i in range(len(switches)-1):
+		net.addLink(switches[i], switches[i+1])
+
+def createTopo():
+	global s
+	if float(n)/s > 24:
+		s += 1
+		createTopo()
+	else:
+		switches = []
+		hosts = []
+		h = 1
+		info('*** Creating topology.\n')		
+		net.addController('c1')
+		if ((float(n)/s)%2 == 0) or n <= 24:
+			for i in range(s):
+				switch = net.addSwitch('s'+str(i+1))
+				switches.append(switch)
+				for j in range(n/s):
+					host = net.addHost('h'+str(h))
+					hosts.append(host)
+					net.addLink(host, switch)
+					h += 1
+		else:
+			num_hosts = n
+			for i in range(s-1):
+
+				switch = net.addSwitch('s'+str(i+1))
+				switches.append(switch)
+				for j in range(n/s):
+					host = net.addHost('h'+str(h))
+					hosts.append(host)
+					net.addLink(host, switch)
+					num_hosts -= 1
+					h += 1
+			switch = net.addSwitch('s'+str(s))
+			switches.append(switch)
+			for i in range(num_hosts):
+				host = net.addHost('h'+str(h))
+				hosts.append(host)
+				net.addLink(host, switch)
+				h += 1
+
+		serial_connection(switches)
+
+def execute():
+	createTopo()
+
+	net.start()
+
+	CLI(net)
+
+	net.stop()
+
+#Main
+if __name__ == '__main__':
+	n = int(sys.argv[1])
+	setLogLevel('info')
+	execute()
